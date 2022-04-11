21702b7e298a6ee8ace22807e60c77a2554c1577
Abraham Aguirre <aguirre.abraham@gmail.com>
2022-04-10 23:49:38 -0400

    trying to populate coins in User portfolio

--- public/javascript/addCoins.js
+++ public/javascript/addCoins.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    // Start counting from the third row
+    // Start counting from the first row
     var counter = 1;
 
     $("#insertRow").on("click", function (event) {
@@ -11,8 +11,8 @@ $(function () {
 
         // Table columns
         cols += '<th scrope="row">' + counter + '</th>';
-        cols += '<td><input class="form-control rounded-0" type="text" name="name" placeholder="Name"></td>';
         cols += '<td><input class="form-control rounded-0" type="text" name="symbol" placeholder="Symbol"></td>';
+        cols += '<td><input class="form-control rounded-0" type="text" name="name" placeholder="Name"></td>';
         cols += '<td><input class="form-control rounded-0" type="text" name="amount" placeholder="Amount"></td>';
         cols += '<td><input class="form-control rounded-0" type="text" name="price" placeholder="Price"></td>';
         cols += '<td><input class="form-control rounded-0" type="text" name="value" placeholder="Value"></td>';
